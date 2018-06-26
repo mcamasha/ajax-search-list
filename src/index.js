edf877ac137499d771ec135ef99d0d22dbbe73d4
mcamasha <mcamasha@gmail.com>
2018-06-26 23:28:15 +0300

    task1

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from 'axios';
+
+const arr = ['Maria', 'Marina', 'Aleksey'];
+
+function Option(props) {
+    return <div><a href="#">{props.value}</a></div>;
+}
+
+class Form extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            input: '',
+            found: []
+        };
+    }
+
+    handleChange(e) {
+        let foundOptions = [];
+        const value = e.target.value.toLowerCase();
+        const self = this;
+        if (value != '') {
+            axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=37662c76ffc19e5cd1b95f37d77155fc&language=ru-RU&sort_by=popularity.desc&page=1`)
+                .then((response) => {
+                    response.data.results.forEach(function (item) {
+                        // if (item.title.toLowerCase().search(value.toLowerCase()) !== -1) {
+                        //     const element = <Option value={item} />;
+                        //     foundOptions.push(element);
+                        // }
+                        if (item.title.toLowerCase().search(value.toLowerCase()) !== -1) {
+                            const element = <Option value={item.title} />;
+                            foundOptions.push(element);
+                        }
+                        self.setState({
+                            found: foundOptions
+                        });
+                    });
+                })
+                .catch((error) => {
+                    console.log('Ошибка: ' + error.message);
+                });
+        }
+        this.setState({
+            input: value
+        });
+    }
+
+    handleClick() {
+        alert("Найдено похожих запросов: " + this.state.found.length);
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <input value={this.state.input} onChange={(e) => this.handleChange(e)} />
+                    <button onClick={this.handleClick}>Click</button>
+                </div>
+                <div>
+                    {this.state.found}
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return <Form />
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
